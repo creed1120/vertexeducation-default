77b72b5b5ad69984f2ca22f83b54af72c7db7ba0
creed1120 <cedricareed@gmail.com>
2022-08-21 09:59:26 -0700

    added bootstrap 4

--- partials/footer/footer-nav.php
+++ partials/footer/footer-nav.php
@@ -12,6 +12,18 @@
 <?php if ($footer_navigation) : ?>
 
     <section id="footer-nav" class="py-4 py-sm-5 background-color-dark-gray">
+
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+        </button>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </div>
+
         <div class="container">
             <div class="row">
                 <?php foreach ($footer_navigation as $column) : ?>
