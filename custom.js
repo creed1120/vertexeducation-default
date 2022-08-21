037bcb511ce7750f51d7d7a64f42a111fd5e2ee7
creed1120 <cedricareed@gmail.com>
2022-08-21 06:01:58 -0700

    added alert script

--- custom.js
+++ custom.js
@@ -1,22 +1,24 @@
-jQuery(document).ready(function( $ ){
+// jQuery(document).ready(function( $ ){
 
-    // function PopUp(){
-    //   document.getElementById('ac-wrapper').style.display="flex";
-    // }
+//     // function PopUp(){
+//     //   document.getElementById('ac-wrapper').style.display="flex";
+//     // }
 
-    $("#ac-wrapper").hide();
+//     $("#ac-wrapper").hide();
 
-    setTimeout(function(){
-          //PopUp();
-       document.getElementById('ac-wrapper').style.visibility="visible";
-         $("#ac-wrapper").fadeIn("slow");
-    },1000); // 5000 to load it after 1 second from page load
+//     setTimeout(function(){
+//           //PopUp();
+//        document.getElementById('ac-wrapper').style.visibility="visible";
+//          $("#ac-wrapper").fadeIn("slow");
+//     },1000); // 5000 to load it after 1 second from page load
 
-    // Popup Close button
-    $("#ffd-close").click(function() {
-        $("#ac-wrapper").fadeOut(500, function() {
-            $("#ac-wrapper").remove();
-        });
-    });
+//     // Popup Close button
+//     $("#ffd-close").click(function() {
+//         $("#ac-wrapper").fadeOut(500, function() {
+//             $("#ac-wrapper").remove();
+//         });
+//     });
 
-});
+// });
+
+alert('Houston we have liftoff!!');
