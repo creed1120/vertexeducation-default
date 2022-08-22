0a524ee38347235a746ec7a082776ec36c13407f
creed1120 <cedricareed@gmail.com>
2022-08-22 09:43:48 -0700

    made changes

--- partials/footer/footer-nav.php
+++ partials/footer/footer-nav.php
@@ -17,7 +17,7 @@
 
         <p>
         <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-            Button with data-target
+            Click Here
         </button>
         </p>
         <div class="collapse" id="collapseExample">
